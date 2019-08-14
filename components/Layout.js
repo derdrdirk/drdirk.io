49809780dfc3e6e9338ee2365cce221b4ce57277
Dirk Hornung <dirkhornung91@gmail.com>
2019-08-14 19:42:05 +0200

    added google analytics

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,31 @@
 import React, {Fragment} from 'react'
 import Footer from './Footer'
 import dynamic from 'next/dynamic'
+import { initGA, logPageView } from '../utils/analytics'
+
 
 const DynamicNavbar = dynamic(
   () => import('./Navbar'),
   { ssr: false }
 )
-const Layout = props => (
-  <Fragment>
-    <DynamicNavbar />
-    <div style={{position: 'relative', top: 40, paddingBottom: 45}}>
-      {props.children}
-    </div>
-    <Footer />
-  </Fragment>
-)
+export default class Layout extends React.Component {
+  componentDidMount () {
+    if (!window.GA_INITIALIZED) {
+      initGA()
+      window.GA_INITIALIZED = true
+    }
+    logPageView()
+  }
 
-export default Layout
+  render() {
+    return (
+      <Fragment>
+        <DynamicNavbar />
+        <div style={{position: 'relative', top: 40, paddingBottom: 45}}>
+          {this.props.children}
+        </div>
+        <Footer />
+      </Fragment>
+    )}
+}
+
